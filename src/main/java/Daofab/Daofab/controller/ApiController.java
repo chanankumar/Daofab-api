a1c3b398e0a51a70be6a8004ba90bd27f49774dc
ChananK <chanancoding@gmail.com>
2023-05-27 00:25:20 +0530

    sorting

--- src/main/java/Daofab/Daofab/controller/ApiController.java
+++ src/main/java/Daofab/Daofab/controller/ApiController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -22,13 +23,20 @@ public class ApiController  {
     @GetMapping("/ParentData")
     public List<Parent> getParentData(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "" + DEFAULT_PAGE_SIZE) int size) throws IOException {
+            @RequestParam(defaultValue = "" + DEFAULT_PAGE_SIZE) int size,
+            @RequestParam(value = "sort", defaultValue = "asc") String sort) throws IOException {
 
         List<Parent> allUsers = getAllParentDataFromJson();
         int startIndex = (page - 1) * size;
         int endIndex = Math.min(startIndex + size, allUsers.size());
         List<Parent> paginatedUsers = allUsers.subList(startIndex, endIndex);
 
+        if (sort.equalsIgnoreCase("asc")) {
+            paginatedUsers.sort(Comparator.comparing(Parent::getId));
+        } else if (sort.equalsIgnoreCase("desc")) {
+            paginatedUsers.sort(Comparator.comparing(Parent::getId).reversed());
+        }
+        
         return paginatedUsers;
 	}
     
